c427e30d8603f21399d32ff87bce8b2f583c6150
Icedevil777 <icedevil777228@mail.ru>
2025-03-05 20:26:16 +0300

    # add FormRow

---
+++ src/components/FormRow.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import IconDelete from './icons/IconDelete.vue'
+import { useAccountStore } from '@/stores/accountStore'
+import { storeToRefs } from 'pinia'
+const accountStore = useAccountStore();
+const deleteData = (id: number) => accountStore.deleteData(id);
+const setNullPass = (type: string, id: number) => accountStore.setNullPass(type, id)
+const { options } = storeToRefs(accountStore);
+
+const selectTarget = ref('')
+const obj = defineModel({type: Object});
+
+const tags = computed({
+  get() {
+    if (Array.isArray(obj.value.tags)) return obj.value.tags.join(';');
+    return null;
+  },
+  set(value) {
+    obj.value.tags = value.length > 0 ? value.split(';') : [];
+  }
+})
+
+const handleBlur = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  if (!input.value.trim()) {
+    input.classList.add('error-color');
+  } else {
+    input.classList.remove('error-color'); 
+  }
+};
+
+const hadlerSelect = (event: Event) => {
+  const target = event.target as HTMLElement;
+  if (target.nodeName === 'SELECT') {
+    selectTarget.value = target;
+    selectTarget.value.classList.remove('error-color');
+  } else {
+    if (event.target.value === '') {
+      selectTarget.value.classList.add('error-color');
+    } else {
+      selectTarget.value.classList.remove('error-color');
+    }
+  }
+};
+</script>
+<template>
+<div class="flex px-1 gap-3 text-xs sm:text-sm">
+    <div class="basis-1/4">
+        <input v-model="tags" @blur="handleBlur" maxlength="50" class="w-full h-full border border-solid rounded-lg px-2" />
+    </div>
+    <div class="basis-1/4">
+        <select @click="hadlerSelect($event)" v-model="obj.type" class="w-full h-full border border-solid rounded-lg px-2 cursor-pointer" >
+        <option value="" >Выберите опцию</option>
+        <option @click="setNullPass(obj.type, obj.id)" v-for="option in options" :key=option.text :value="option.text"> {{ option.text }} </option>
+        </select>
+    </div>
+    <div class="basis-1/4">
+        <input v-model="obj.login" @blur="handleBlur" maxlength="100" class="w-full h-full border border-solid rounded-lg px-2" />
+    </div>
+    <div class="basis-1/4">
+        <input v-show="obj.type != 'Local'" @blur="handleBlur" v-model="obj.pass" type="password" maxlength="50" class="w-full h-full border border-solid rounded-lg px-2" />
+    </div>
+    <div class="flex basis-1 sm:justify-start justify-end">
+        <IconDelete @click="deleteData(obj.id)" class="size-3 sm:size-5 cursor-pointer mt-1"/>
+    </div>
+</div>
+</template>
+
+
+<style scoped>
+
+.error-color {
+  border-color: red;
+}
+</style>
--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,48 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import IconDelete from './icons/IconDelete.vue'
 import IconPlus from './icons/IconPlus.vue'
 import IconSign from './icons/IconSign.vue'
+import FormRow from './FormRow.vue'
 import { useAccountStore } from '@/stores/accountStore'
 import { storeToRefs } from 'pinia'
 
 const accountStore = useAccountStore();
-const { data, options } = storeToRefs(accountStore);
-
+const { data } = storeToRefs(accountStore);
 const addData = () => accountStore.addData();
-const deleteData = (id: number) => accountStore.deleteData(id);
-const setNullPass = (type: string, id: number) => accountStore.setNullPass(type, id)
-
-const message = ref('');
-const selectTarget = ref('')
-
-const handleBlur = (event: Event) => {
-  const input = event.target as HTMLInputElement;
-  if (!input.value.trim()) {
-    message.value = 'Поле не может быть пустым!';
-    input.classList.add('error-color');
-  } else {
-    message.value = '';
-    input.classList.remove('error-color'); 
-  }
-};
-
-const hadlerSelect = (event: Event) => {
-  const target = event.target as HTMLElement;
-  if (target.nodeName === 'SELECT') {
-    selectTarget.value = target;
-    selectTarget.value.classList.remove('error-color');
-  } else {
-    if (event.target.value === '') {
-      message.value = 'Необходимо сделать выбор!';
-      selectTarget.value.classList.add('error-color');
-    } else {
-      message.value = '';
-      selectTarget.value.classList.remove('error-color');
-    }
-  }
-};
-
 </script>
 
 <template>
@@ -53,14 +18,12 @@ const hadlerSelect = (event: Event) => {
         <p class="sm:text-3xl ">Учетные записи</p>
         <IconPlus class="" @click="addData"/>
       </div>
-      <p class="sm:flex hidden self-center text-red-600 text-xs sm:text-sm"> {{ message }} </p>
     </div>  
 
     <div class="flex flex-row gap-x-1 w-full text-xs sm:text-sm">
       <IconSign />
       <p class="self-center">Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</p>
     </div>
-    <p class="flex sm:hidden text-red-600 text-xs sm:text-sm mb-1"> {{ message }} </p>
     <div class="space-y-3 text-xs sm:text-sm border-solid border p-3 rounded-xl">
       <div class="flex  px-1">
           <div class="basis-1/4">
@@ -80,28 +43,9 @@ const hadlerSelect = (event: Event) => {
             <div class="w-2 sm:w-4"></div>
           </div>
       </div>
-      <div v-for="obj in data" :key="obj.id" class="flex px-1 gap-3 text-xs sm:text-sm">
-        <div class="basis-1/4">
-          <input v-model="obj.tags" @blur="handleBlur" maxlength="50" class="w-full h-full border border-solid rounded-lg px-2" />
-        </div>
-        <div class="basis-1/4">
-          <select @click="hadlerSelect($event)" v-model="obj.type" class="w-full h-full border border-solid rounded-lg px-2 cursor-pointer" >
-            <option value="" >Выберите опцию</option>
-            <option @click="setNullPass(obj.type, obj.id)" v-for="option in options" :key=option.text :value="option.text"> {{ option.text }} </option>
-          </select>
-        </div>
-        <div class="basis-1/4">
-          <input v-model="obj.login" @blur="handleBlur" maxlength="100" class="w-full h-full border border-solid rounded-lg px-2" />
-        </div>
-        <div class="basis-1/4">
-          <input v-show="obj.type != 'Local'" @blur="handleBlur" v-model="obj.pass" type="password" maxlength="50" class="w-full h-full border border-solid rounded-lg px-2" />
-        </div>
-        <div class="flex basis-1 sm:justify-start justify-end">
-          <IconDelete @click="deleteData(obj.id)" class="size-3 sm:size-5 cursor-pointer mt-1"/>
-        </div>
-      </div>
+      <FormRow v-for="(obj, key) in data" :key="obj.id" v-model="data[key]" />
     </div>
-    {{ data }}
+    {{ data[0].tags }}
   </div>
 </template>
 
